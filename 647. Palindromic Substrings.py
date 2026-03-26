ff24e9378971062ac239b0466fdb1d687ce5880c
Mihiran Piyarathna <sasindumihiran@gmail.com>
2026-03-26 23:59:54 +0000

    message title
    optional message desc

--- 647. Palindromic Substrings.py
+++ 647. Palindromic Substrings.py
@@ -29,3 +29,54 @@ Constraints:
 1 <= s.length <= 1000
 s consists of lowercase English letters.
 """
+# sliding window question
+
+s = "abc"
+s = "aaa"
+
+
+def countSubstrings(s):
+    #
+    
+        def ispalin(s):
+            if len(s)<1:
+                return False
+            for i in range (len(s)//2):
+                if s[i] == s[-1-i]:
+                    pass
+                else:
+                    return False
+            return True
+        
+        # print(ispalin(s))
+        s_len = len(s)
+        cnt=0
+        palincnt=0
+        a=0
+        b=1
+        """
+        below usual rubber band sliding window not exhaustive
+        have to resort to o(n**2) 
+
+        while b<= s_len:
+            if ispalin(s[a:b]):
+                print(s[a:b], " ",a, " ",b)
+                palincnt +=1
+                if b-a <=1:
+                    b +=1
+                else:
+                    a +=1
+                
+            else:
+                b +=1
+        """
+        for i in range(len(s)):
+            inum = s[i]
+            # print(inum)
+            for j in range(i,s_len):
+                if ispalin(s[i:j+1]):
+                    # print(s[i:j+1])
+                    palincnt +=1
+        return palincnt
+
+countSubstrings(s)
