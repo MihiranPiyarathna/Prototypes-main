9bd23515cef1ad303283b6b1cb4c53563e551a4d
Mihiran Piyarathna <sasindumihiran@gmail.com>
2026-05-17 22:03:44 +0100

    message title
    optional message desc

--- coderbyte Stock Picker - Data Science.py
+++ coderbyte Stock Picker - Data Science.py
@@ -1,3 +1,18 @@
 """
 
 """
+arr = [10,12,4,5,9]
+
+def StockPicker(arr):
+    if len(arr)<2: return -1
+    buy = arr[0]
+    max_profit = -1
+    for p in arr[1:]:
+        if p<buy : 
+            buy = p
+        else:
+            max_profit = max(p-buy, max_profit)
+    return max_profit
+
+StockPicker(arr)
+# pass
